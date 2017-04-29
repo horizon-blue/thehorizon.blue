c949242411f1ff4efb2116b213c78dde97c4ed47
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-04-28 21:15:06 -0500

    Update testing

--- test/test.py
+++ test/test.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+moment = Moment(app)
+bootstrap = Bootstrap(app)
+
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Test</h1>"
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
