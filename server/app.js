8f604626ebf1a2ead759150274e4e67d029889cb
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-06-21 20:25:23 -0500

    Reset server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import * as express from 'express';
+import * as path from 'path';
+import morgan from 'morgan';
+
+const app = express.default();
+
+// Setup logger
+app.use(
+    morgan(
+        ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
+    )
+);
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+export default app;
---
+++ server/index.js
@@ -0,0 +1,8 @@
+'use strict';
+import app from './app';
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
