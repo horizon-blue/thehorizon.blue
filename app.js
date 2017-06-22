1a8f9a60ad7e93570d40348958ac4c5555975b12
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-06-21 19:27:42 -0500

    Save current setting before installing other packages

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express.default();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(5000);
