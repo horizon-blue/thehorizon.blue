359f7b4382ab294655003b3c5e2d897b5350c732
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-06-23 23:37:57 -0500

    Forgot to add div style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import classnames from 'classnames';
 import Router from './Router';
 import Header from './components/Header';
 
 class App extends Component {
     render() {
+        const { className, ...props } = this.props;
         return (
             <BrowserRouter>
-                <div>
+                <div className={classnames('App', className)} {...props}>
                     <Header />
                     <Router />
                 </div>
