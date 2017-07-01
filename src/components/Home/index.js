c82be3968a16a7eb0675ce96354aa4ae73dee8fa
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-07-01 04:24:10 -0500

    Add animation for home page && add decorator plugin

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import { gql, graphql } from 'react-apollo';
 import { bind as Mousetrap } from 'mousetrap';
+import Anime from 'react-anime';
 
 import './style.css';
 
-const testQuery = gql`query { test }`;
-
+@graphql(gql`query { test }`)
 class Home extends Component {
   componentWillMount() {
     // The special hotkeys to enter the main website
-    Mousetrap('up up down down left right left right b a enter', () =>
+    Mousetrap('up down left right up down left right', () =>
       this.props.history.push('/about')
     );
   }
@@ -19,21 +19,23 @@ class Home extends Component {
 
     return (
       <div className={classnames('Home', className)}>
-        <p className="App-intro">
-          之前一直想写一个<del>酷炫</del>的个人网站，然而一直未果。正好最近在折腾<code>React Native</code>，就想着顺手用<code>React</code>把天际林整理一下好了。
-        </p>
-        <p className="App-intro">
-          你现在看到的只是<del>又一个</del>占位置的首页，具体内容最近会抽空更新。在此之前，欢迎去原博客<a href="http://www.forestofhorizon.com/">天际林</a>转转。
-        </p>
-        <p className="App-intro">
-          回见。
-        </p>
-        <p className="App-intro">
-          — 蓝色天际 | {test} —
-        </p>
+        <Anime opacity={[0, 1]} translateY={'1em'} delay={(e, i) => i * 500}>
+          <p className="App-intro">
+            之前一直想写一个<del>酷炫</del>的个人网站，然而一直未果。正好最近在折腾<code>React Native</code>，就想着顺手用<code>React</code>把天际林整理一下好了。
+          </p>
+          <p className="App-intro">
+            你现在看到的只是<del>又一个</del>占位置的首页，具体内容最近会抽空更新。在此之前，欢迎去原博客<a href="http://www.forestofhorizon.com/">天际林</a>转转。
+          </p>
+          <p className="App-intro">
+            回见。
+          </p>
+          <p className="App-intro">
+            — 蓝色天际 | {test} —
+          </p>
+        </Anime>
       </div>
     );
   }
 }
 
-export default graphql(testQuery)(Home);
+export default Home;
