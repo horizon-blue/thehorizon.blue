f4c498e5c60316e2f80c6c9d67d5b83faaae7bab
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-07-14 04:11:48 -0500

    Fix protocol

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,7 @@ export const UPLOAD_PHOTO = process.env.API_SERVER === 'test'
     : 'https://api.thehorizon.blue/upload';
 
 export const WEB_ROOT = 'https://thehorizon.blue';
-export const IMG_ROOT = 'http://horizonblue.oss-us-west-1.aliyuncs.com/img/';
+export const IMG_ROOT = 'https://horizonblue.oss-us-west-1.aliyuncs.com/img/';
 export const POST_ROOT = `${WEB_ROOT}/blog/post/`;
 
 export const DRAFT_VISIBILITY_ID = 4;
